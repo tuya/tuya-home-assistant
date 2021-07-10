2eb1cf2baeb457a896b7a3f8e61664b4a1a353d0
Michal Duda <github@vookimedlo.cz>
2021-07-10 15:54:40 +0200

    🔩Code: Redundant code removed

--- custom_components/tuya_v2/fan.py
+++ custom_components/tuya_v2/fan.py
@@ -240,8 +240,6 @@ class TuyaHaFan(TuyaHaDevice, FanEntity):
             supports = supports | SUPPORT_OSCILLATE
         if DPCODE_FAN_DIRECTION in self.tuya_device.status:
             supports = supports | SUPPORT_DIRECTION
-        if DPCODE_FAN_SPEED in self.tuya_device.status:
-            supports = supports | SUPPORT_SET_SPEED
            
         # Air Purifier specific
         if DPCODE_AP_FAN_SPEED in self.tuya_device.status or DPCODE_AP_FAN_SPEED_ENUM in self.tuya_device.status:
