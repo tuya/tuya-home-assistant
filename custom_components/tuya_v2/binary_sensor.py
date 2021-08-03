1a293cd7b2816c54d9eda2326e7e942ca0ed5ba8
乾启 <18442047+tsutsuku@users.noreply.github.com>
2021-08-03 10:32:40 +0800

    [fix] the binary sensor does not generate unique IDs when multi same type in one device

--- custom_components/tuya_v2/binary_sensor.py
+++ custom_components/tuya_v2/binary_sensor.py
@@ -108,6 +108,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_DOOR,
+                    DPCODE_DOORLOCK_STATE,
                     (lambda d: d.status.get(DPCODE_DOORLOCK_STATE, "none") != "closed"),
                 )
             )
@@ -117,6 +118,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_DOOR,
+                    DPCODE_DOORCONTACT_STATE,
                     (lambda d: d.status.get(DPCODE_DOORCONTACT_STATE, False)),
                 )
             )
@@ -126,6 +128,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_DOOR,
+                    DPCODE_SWITCH,
                     (lambda d: d.status.get(DPCODE_SWITCH, False)),
                 )
             )
@@ -135,6 +138,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_SMOKE,
+                    DPCODE_SMOKE_SENSOR_STATE,
                     (lambda d: d.status.get(DPCODE_SMOKE_SENSOR_STATE, 1) == "1"),
                 )
             )
@@ -144,6 +148,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_SMOKE,
+                    DPCODE_SMOKE_SENSOR_STATUS,
                     (lambda d: d.status.get(DPCODE_SMOKE_SENSOR_STATUS, 'normal') == "alarm"),
                 )
             )
@@ -153,6 +158,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_BATTERY,
+                    DPCODE_BATTERY_STATE,
                     (lambda d: d.status.get(DPCODE_BATTERY_STATE, 'normal') == "low"),
                 )
             )
@@ -162,6 +168,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOTION,
+                    DPCODE_TEMPER_ALRAM,
                     (lambda d: d.status.get(DPCODE_TEMPER_ALRAM, False)),
                 )
             )
@@ -171,6 +178,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_GAS,
+                    DPCODE_GAS_SENSOR_STATE,
                     (lambda d: d.status.get(DPCODE_GAS_SENSOR_STATE, 1) == "1"),
                 )
             )
@@ -180,6 +188,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOTION,
+                    DPCODE_PIR,
                     (lambda d: d.status.get(DPCODE_PIR, "none") == "pir"),
                 )
             )
@@ -189,6 +198,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOISTURE,
+                    DPCODE_WATER_SENSOR_STATE,
                     (lambda d: d.status.get(DPCODE_WATER_SENSOR_STATE, "normal") == "alarm"),
                 )
             )
@@ -198,6 +208,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_PROBLEM,
+                    DPCODE_SOS_STATE,
                     (lambda d: d.status.get(DPCODE_SOS_STATE, False)),
                 )
             )
@@ -207,6 +218,7 @@ def _setup_entities(hass, device_ids: List):
                     device,
                     device_manager,
                     DEVICE_CLASS_MOTION,
+                    DPCODE_PRESENCE_STATE,
                     (
                         lambda d: d.status.get(DPCODE_PRESENCE_STATE, "none")
                         == "presence"
@@ -225,22 +237,24 @@ class TuyaHaBSensor(TuyaHaDevice, BinarySensorEntity):
         device: TuyaDevice,
         device_manager: TuyaDeviceManager,
         sensor_type: str,
+        sensor_code: str,
         sensor_is_on: Callable[..., bool],
     ):
         """Init TuyaHaBSensor."""
         self._type = sensor_type
+        self._code = sensor_code
         self._is_on = sensor_is_on
         super().__init__(device, device_manager)
 
     @property
     def unique_id(self) -> Optional[str]:
         """Return a unique ID."""
-        return f"{super().unique_id}{self._type}"
+        return f"{super().unique_id}{self._code}"
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self.tuya_device.name + "_" + self._type
+        return self.tuya_device.name + "_" + self._code
 
     @property
     def is_on(self):
