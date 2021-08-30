18224aa5de21bf688f668c39bf9927a77eeb8fe5
dougiteixeira <31328123+dougiteixeira@users.noreply.github.com>
2021-08-30 18:46:02 -0300

    Adds: Robot vacuum localte, status and attributes
    Add robot vacuum locate function, paused and docking status and attributes clean area, time, status and clean record.

--- custom_components/tuya_v2/vacuum.py
+++ custom_components/tuya_v2/vacuum.py
@@ -52,7 +52,11 @@ DPCODE_PAUSE = "pause"
 DPCODE_RETURN_HOME = "switch_charge"
 
 DPCODE_BATTERY = "electricity_left"
-
+DPCODE_LOCATE = "seek"
+DPCODE_STATUS_FULL = "status_full"
+DPCODE_CLEAN_AREA = "clean_area"
+DPCODE_CLEAN_TIME = "clean_time"
+DPCODE_CLEAN_RECORD = "clean_record"
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
@@ -109,7 +113,23 @@ class TuyaHaVacuum(TuyaHaDevice, StateVacuumEntity):
     def battery_level(self) -> int | None:
         """Return Tuya device state."""
         return self.tuya_device.status.get(DPCODE_BATTERY)
-
+		
+    @property
+    def device_state_attributes(self):
+        """Return the optional state attributes with device specific additions."""
+        attr = {}
+        if self.tuya_device.status.get(DPCODE_MODE):
+          attr[DPCODE_MODE] = self.tuya_device.status.get(DPCODE_MODE)
+        if self.tuya_device.status.get(DPCODE_STATUS):
+          attr[DPCODE_STATUS_FULL] = self.tuya_device.status.get(DPCODE_STATUS)
+        if self.tuya_device.status.get(DPCODE_CLEAN_AREA):
+          attr[DPCODE_CLEAN_AREA] = self.tuya_device.status.get(DPCODE_CLEAN_AREA)
+        if self.tuya_device.status.get(DPCODE_CLEAN_TIME):
+          attr[DPCODE_CLEAN_TIME] = self.tuya_device.status.get(DPCODE_CLEAN_TIME)
+        if self.tuya_device.status.get(DPCODE_CLEAN_RECORD):
+          attr[DPCODE_CLEAN_RECORD] = self.tuya_device.status.get(DPCODE_CLEAN_RECORD)
+        return attr
+		
     @property
     def state(self):
         """Return Tuya device state."""
@@ -123,10 +143,12 @@ class TuyaHaVacuum(TuyaHaDevice, StateVacuumEntity):
 
         if status == "standby":
             return STATE_IDLE
-        if status == "goto_charge":
+        if status == "goto_charge" or status == "docking":
             return STATE_RETURNING
         if status == "charging" or status == "charge_done" or status == "chargecompleted":
             return STATE_DOCKED
+        if status == "pause":
+            return STATE_PAUSED
         return STATE_CLEANING
 
     @property
@@ -193,3 +215,8 @@ class TuyaHaVacuum(TuyaHaDevice, StateVacuumEntity):
         """Return device to Dock"""
         _LOGGER.debug(f"Return to base device {self.name}")
         self._send_command([{"code": DPCODE_MODE, "value": "chargego"}])
+		
+    def locate(self, **kwargs: Any) -> None:
+        """Return device to Dock"""
+        _LOGGER.debug(f"Locate the device {self.name}")
+        self._send_command([{"code": DPCODE_LOCATE, "value": True}])
