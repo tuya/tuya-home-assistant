b89949d7367f162c4280577118cf71ae8429e54e
some-guy-in-oz <85833124+some-guy-in-oz@users.noreply.github.com>
2021-09-29 09:39:39 +0930

    Fix range

--- custom_components/tuya_v2/fan.py
+++ custom_components/tuya_v2/fan.py
@@ -154,12 +154,13 @@ class TuyaHaFan(TuyaHaDevice, FanEntity):
                 else:
                     # No enum, use values
                     self.speed_style_enum = False
-                    dp_range = json.loads(self.tuya_device.function.get(self.dp_code_speed_function).values)
+                    dp_range = json.loads(self.tuya_device.function.get(self.dp_code_speed_function, {}).values)
                     self.speed_min = dp_range.get("min", 1)
                     self.speed_max = dp_range.get("max", 100)
-                    self.fan_speed_count = int_states_in_range(self.speed_min, self.speed_max)
+                    self.fan_speed_count = int_states_in_range((self.speed_min, self.speed_max))
             except Exception:
                 _LOGGER.error("Cannot parse the legacy speed range")
+                raise
 
         # Special handling for "kj" device (air purifier) - can probably be merged into above code but left here for now
         if self.tuya_device.category == "kj":
